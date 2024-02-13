8317b1067104e6282224fd403ae03e90f78e487f
DevHamzaKhaliq198 <zoiwli198@gmail.com>
2024-02-13 13:49:37 -0800

    Requirements fulfilled

---
+++ index.js
@@ -0,0 +1,17 @@
+import inquirer from "inquirer";
+const random = Math.floor(Math.random() * 10);
+const result = await inquirer.prompt([
+    {
+        name: "userNumber",
+        type: "number",
+        message: "Guess a Number from 1 to 10"
+    }
+]);
+const { userNumber } = result;
+if (userNumber === random) {
+    console.log("Hurrah! You won the game");
+}
+else {
+    console.log("Guess again");
+}
+console.log("system generated number is:", random);
